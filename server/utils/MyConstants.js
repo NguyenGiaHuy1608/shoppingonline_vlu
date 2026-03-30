d9893b7043f098a866cc86a5817a105277215f23
NguyenGiaHuy1608 <nguyengiahuy9a2qt@gmail.com>
2026-03-30 10:39:17 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,14 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.be2vnal.mongodb.net',
+  DB_USER: 'huygianguyen1608_db_user',
+  DB_PASS: '16083719',
+  DB_DATABASE: 'shoppingonline',
+
+  EMAIL_USER: 'huygianguyen1608@gmail.com',
+  EMAIL_PASS: 'vjtwkeppjtvzfveq',
+
+  JWT_SECRET: 'abciooiqwe',
+  JWT_EXPIRES: '356658435644111',
+};
+
+module.exports = MyConstants;
